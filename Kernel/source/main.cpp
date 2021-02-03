81129590336a38691acf974ad92dc6f1b807f702
OSM-Made <oldskoolmodz@gmail.com>
2021-02-03 01:31:36 -0700

    remove testing stuff.

--- Kernel/source/main.cpp
+++ Kernel/source/main.cpp
@@ -26,16 +26,6 @@ extern "C" void _main(uint64_t* p)
 
 	DebugLog(LOGTYPE_INFO, "API Loaded!!!\nProcess Functions: %d\nDebugging Functions %d\nKernel Functions: %d\nTarget Functions: %d\nTotal API Calls: %d", (PROC_END - PROC_START) - 2, (DBG_END - DBG_START) - 2, (KERN_END - KERN_START) - 2, (TARGET_END - TARGET_START) - 2, TARGET_END - 8);
 
-	//Enable UART
-	//uint8_t Uart = 1;
-	//icc_nvs_write(4, 0x31F, 0x1, &Uart);
-
-	uint8_t Uart = 0;
-	icc_nvs_write(4, 0x1600, 0x1, &Uart);
-
-	//for(int i = 0; i < 6; i++)
-	//	Log("Value = %d%%\n", *(int32_t*)(resolve(0x2381C48) + (i * 0x4)));
-
 	pHelperManager->pUserlandHelper->sceSysUtilSendNotificationRequest("OrbisLib v%i.%i Loaded!\nMade by OSM", ORBISLIB_MAJOR_VERSION, ORBISLIB_MINOR_VERSION);
 
 	kprintf("Hello world from OrbisLib!!!\n");
